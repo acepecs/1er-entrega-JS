8e81fe96ab278d7a4376ae8bbf414f3a59461be7
martin.acerenza <acerenzamartin@gmail.com>
2022-03-16 23:44:46 -0300

    array

--- sitio.js
+++ sitio.js
@@ -1,5 +1,25 @@
-let products = [];
+
 let total = 0;
+
+let product1 = [id: 1, "Zapatillas Adidas", precio:15000] 
+let product2 = [id: 2, "Zapatillas Salomon", precio: 12000]
+let product3 = [id: 3, "Zapatillas Nike", precio: 18000]
+let product4 = [id: 4, "Zapatillas Under Armour", precio: 14000];
+
+agregarAlCarrito(producto1);
+console.log(carrito);
+agregarAlCarrito(producto2);
+console.log(carrito);
+agregarAlCarrito(producto3);
+console.log(carrito);
+agregarAlCarrito(producto4);
+console.log(carrito);
+
+function agregarAlCarrito(producto){
+
+carrito.push(producto);
+}
+
 
 function add(product,price){
     console.log(product,price);
